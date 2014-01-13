92ee84d487b4e50ccd93bb51c99babad7ff749f3
dmitry <ovcharenkodmitry@gmail.com>
2014-01-12 16:27:23 -0800

    add controller and views to model picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,8 @@ class Picture < ActiveRecord::Base
   require 'carrierwave/orm/activerecord'
   mount_uploader :avatar, AvatarUploader
 
+
+
   belongs_to :imageable, polymorphic: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Auction::Application.routes.draw do
     resources :categories
   end
 
+  resources :pictures
+
   root 'sites#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
