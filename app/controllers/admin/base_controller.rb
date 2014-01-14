08f829488091db7968fe43233272ad810fd21614
dmitry <ovcharenkodmitry@gmail.com>
2014-01-14 06:10:46 -0800

    add admin fileld to users

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,6 +4,10 @@ class Admin::BaseController < ApplicationController
   #before_action :authenticate_user!
   #before_action :require_admin
 
+
+  before_action :authenticate_user!
+  before_action :require_admin
+
   layout "admin"
 
   private
