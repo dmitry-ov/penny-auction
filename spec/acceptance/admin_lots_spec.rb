2a1bfe4aed501cdf0066378aaf6a98b596de2e7b
dmitry <ovcharenkodmitry@gmail.com>
2014-01-21 14:09:35 +0400

    rename files

--- spec/acceptance/admin_lots_spec.rb
+++ spec/acceptance/admin_lots_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-feature "Admin can manage products", %q{
-  In order to sell the products
+feature "Admin can manage lots", %q{
+  In order to sell the lots
   As an admin
-  I want to be able to manage products
+  I want to be able to manage lots
  } do
 
-  let(:path) { admin_products_path }
+  let(:path) { admin_lots_path }
 
   background do
     User.create!(email: 'admin@test.com', password: '12345678', password_confirmation: '12345678', admin: true)
@@ -14,7 +14,7 @@ feature "Admin can manage products", %q{
 
   it_behaves_like "Admin accessible"
 
-  scenario 'Admin can view products list' do
+  scenario 'Admin can view lots list' do
     pending
   end
 
