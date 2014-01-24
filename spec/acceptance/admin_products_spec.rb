91aa199515175693ddc2a24efdea0fc2372c4045
dmitry <ovcharenkodmitry@gmail.com>
2014-01-24 14:53:24 +0400

    acceptance admin product user history

--- spec/acceptance/admin_products_spec.rb
+++ spec/acceptance/admin_products_spec.rb
@@ -7,26 +7,68 @@ feature "Admin can manage products", %q{
  } do
 
   let(:path) { admin_products_path }
+  let(:title) { 'mobile phone' }
+  let(:description) { 'new iphone' }
+  let(:price) { 1234567.89 }
 
   background do
     User.create!(email: 'admin@test.com', password: '12345678', password_confirmation: '12345678', admin: true)
+    Product.create!(title: title, description: description, price: price)
   end
 
   it_behaves_like "Admin accessible"
 
+    feature 'Admin can' do
 
-  describe 'Admin can' do
-    pending 'view products list'
-    pending 'add product'
-    pending 'show product'
-    pending 'edit product'
-    pending 'destroy product'
-    pending 'click back on new page'
-    pending 'click back on show page'
-    pending 'click edit on show page'
-    pending 'click show on edit page'
-    pending 'click back on edit page'
-  end
+      background do
+        visit path
+        sign_in_with 'admin@test.com', '12345678'
+      end
+
+      scenario 'view products list' do
+        page.should have_content 'Управление продуктами'
+        page.should have_content title
+        page.should have_content description
+        page.should have_content price
+      end
+
+      scenario 'add product' do
+        click_on 'New Product'
+        page.should have_content 'Title'
+        page.should have_content 'Description'
+        page.should have_content 'Price'
+        page.should have_content 'Category'
+        page.should have_button 'Save'
+        page.should have_link 'Add picture'
+        page.should have_link 'Back'
+      end
+
+      scenario 'show product' do
+        click_on 'Show'
+        page.should have_content title
+        page.should have_content description
+        page.should have_content price
+        page.should have_link 'Edit'
+        page.should have_link 'Back'
+      end
+
+      scenario 'edit product' do
+        click_on 'Edit'
+        page.should have_field('Title', with: title)
+        page.should have_field('Description', with: description)
+        page.should have_field('Price', with: price)
+        page.should have_button 'Save'
+        page.should have_link 'Show'
+        page.should have_link 'Back'
+      end
+
+      scenario 'destroy product' do
+        click_on 'Destroy'
+        page.should_not have_content title
+        page.should_not have_content description
+        page.should_not have_content price
+      end
+    end
 
 
 end
