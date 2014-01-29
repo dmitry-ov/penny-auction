86e5628cf98cae95fc4d200744282ad6aba8e7b6
dmitry <ovcharenkodmitry@gmail.com>
2014-01-29 14:45:46 +0400

    product controller include inherited_resources

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,76 +1,22 @@
 class Admin::ProductsController < Admin::BaseController
-  before_action :set_admin_product, only: [:show, :edit, :update, :destroy]
-  before_action :get_admin_categories, only: [:new, :show, :edit, :update]
+  inherit_resources
 
-  # GET /admin/products
-  def index
-    @admin_products = Product.all
-  end
+  respond_to :html
+  #respond_to :js, only: [:create, :destroy]
 
-  # GET /admin/products/1
-  def show
-  end
+  before_action :build_resource, only: [:index, :new]
+  #before_action :collection, only: [:create, :destroy], if: -> { request.xhr? }
 
-  # GET /admin/products/new
-  def new
-    @admin_product = Product.new
-  end
-
-  # GET /admin/products/1/edit
-  def edit
-  end
-
-  # POST /admin/products
-  # POST /admin/products.json
-  def create
-    @admin_product = Product.new(admin_product_params)
-    respond_to do |format|
-      if @admin_product.save
-        format.html { redirect_to [:admin, @admin_product], notice: 'Product was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @admin_product }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /admin/products/1
-  # PATCH/PUT /admin/products/1.json
-  def update
-    respond_to do |format|
-      if @admin_product.update(admin_product_params)
-        format.html { redirect_to [:admin, @admin_product], notice: 'Product was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /admin/products/1
-  # DELETE /admin/products/1.json
-  def destroy
-    @admin_product.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_products_url }
-      format.json { head :no_content }
-    end
-  end
+  before_action :admin_categories, only: [:new, :show, :edit, :update]
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_product
-      @admin_product = Product.find(params[:id])
-    end
 
-    def get_admin_categories
-      @admin_categories = Category.all
-    end
+  def admin_categories
+    @admin_categories = Category.all
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def admin_product_params
-      params[:product].permit(:title, :description, :price, :category_id, :avatar, pictures_attributes: [:id, :avatar, :_destroy])
-    end
+  def build_resource_params
+    [params.fetch(:product, {}).permit(:title, :description, :price,:category_id, :avatar, pictures_attributes: [:id, :avatar, :_destroy])]
+  end
+
 end
