32c12829ea3c61c36930a4c1e6ec6902daecfe49
dmitry <ovcharenkodmitry@gmail.com>
2014-02-22 23:49:47 +0400

    main page with all lots

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -13,9 +13,9 @@ class Lot < ActiveRecord::Base
   #scope :started, -> { where ("begin_date < #{Time.now}")}
   #scope :red, -> { where(color: 'red') }
 
-  scope :started, -> { Lot.all { |lot| lot.started? } }
-  scope :active, -> { Lot.all { |lot| lot.active? } }
-  scope :finished, -> { Lot.all { |lot| lot.finished? } }
+  scope :started, -> { Lot.all.select { |lot| lot.started? } }
+  scope :active, -> { Lot.all.select { |lot| lot.active? } }
+  scope :finished, -> { Lot.all.select { |lot| lot.finished? } }
 
   def started?
     self.begin_date < Time.now.utc
