b26df5f12ed4cfaee6c2343e9a70eb996a2d527a
dmitry <ovcharenkodmitry@gmail.com>
2014-03-04 18:45:58 +0400

    set default value to betscount in user model

--- db/migrate/20140304135454_add_betscount_to_user.rb
+++ db/migrate/20140304135454_add_betscount_to_user.rb
@@ -1,5 +1,5 @@
 class AddBetscountToUser < ActiveRecord::Migration
   def change
-    add_column :users, :betscount, :integer
+    add_column :users, :betscount, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20140304135454) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
-    t.integer  "betscount"
+    t.integer  "betscount",              default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
