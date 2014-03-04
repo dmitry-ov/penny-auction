893106e8bc5106c5391681b40d142b52ef5fcdff
dmitry <ovcharenkodmitry@gmail.com>
2014-03-04 23:37:17 +0400

    fix spec

--- spec/acceptance/main_page_spec.rb
+++ spec/acceptance/main_page_spec.rb
@@ -8,9 +8,9 @@ feature 'User show list of lots' do
     Product.create!(title: 'cellular phone', description: 'new iphone 5s', price: 14567.89, category: Category.first)
     Product.create!(title: 'radio phone', description: 'new iphone 5s', price: 34567.89, category: Category.first)
 
-    Lot.create!(step_price: 0.02, price: 1511.14, product: Product.first, begin_date: DateTime.now + 1.day, expire_date: DateTime.now + 2.day )
+    Lot.create!(step_price: 0.02, price: 1511.14, product: Product.find_by_title('mobile phone'), begin_date: DateTime.now + 1.day, expire_date: DateTime.now + 2.day )
     Lot.create!(step_price: 0.01, price: 3544.54, product: Product.find_by_title('cellular phone'), begin_date: DateTime.now - 1.day, expire_date: DateTime.now + 1.day )
-    Lot.create!(step_price: 0.03, price: 555.39, product: Product.last, begin_date: DateTime.now - 2.day, expire_date: DateTime.now - 1.day )
+    Lot.create!(step_price: 0.03, price: 555.39, product: Product.find_by_title('radio phone'), begin_date: DateTime.now - 2.day, expire_date: DateTime.now - 1.day )
   end
 
 
@@ -27,10 +27,10 @@ feature 'User show list of lots' do
     page.should have_content 'Время окончания аукциона'
     page.should have_content 'Шаг цены'
 
+    save_and_open_page
     page.should have_content 'mobile phone'
     page.should have_content 'cellular phone'
     page.should have_content 'radio phone'
 
-    #page.should have_link 'show'
   end
 end
