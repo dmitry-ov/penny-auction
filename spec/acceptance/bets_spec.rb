27177ddd9e771fa51d292d9c918389677ea9d4ba
dmitry <ovcharenkodmitry@gmail.com>
2014-03-26 17:52:05 +0400

    spec from redirect guest user

--- spec/acceptance/bets_spec.rb
+++ spec/acceptance/bets_spec.rb
@@ -1,6 +1,6 @@
 require 'acceptance/acceptance_helper'
 
-feature 'bet create ' do
+feature 'bet create' do
   let(:button) { 'Make a Bet!' }
 
   before do
@@ -11,20 +11,31 @@ feature 'bet create ' do
     Lot.create!(step_time: 1, step_price: 0.02, price: 1511.14, product: Product.first, begin_date: DateTime.now - 1.day, expire_date: DateTime.now + 2.day )
   end
 
-  feature "user not create bet", js: true do
+  feature "redirect if guest make bet", js: true  do
+    background do
+      visit "/lots/#{Lot.first.id}"
+    end
+
+    scenario "redirect" do
+      click_button button
+      expect(page).to have_content 'Список аукционов'
+    end
+  end
+
+    feature "user have not betscount", js: true do
     background do
       visit '/users/sign_in'
       sign_in_with 'user@test.com', '12345678'
       visit "/lots/#{Lot.first.id}"
     end
 
-    scenario "have not betscount" do
+    scenario "not create bet" do
       click_button button
       expect(page).to have_content 'У вас нет ставок'
     end
   end
 
-  feature "user create bet" do
+  feature "user have betscount" do
     let(:lot_path) { "/lots/#{Lot.first.id}" }
 
     background do
@@ -34,7 +45,7 @@ feature 'bet create ' do
       visit lot_path
     end
 
-    scenario "have betscount" do
+    scenario "create bet" do
       expect(page).to have_content 'Bets 1'
       click_button button
       visit lot_path
